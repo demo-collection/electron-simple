b1f3049c785a3588441d006ef8b7c9119d2b1d54
yanlele <331393627@qq.com>
2020-02-01 00:03:42 +0800

    feat: 关闭窗口

--- src/core/window/index.ts
+++ src/core/window/index.ts
@@ -1,10 +1,20 @@
-const openWindow = () => window.open('./pages/pop_page.html', 'pop_page');
+let subWin: Window;
+
+const openWindow = () => {
+  subWin = window.open('./pages/pop_page.html', 'pop_page');
+};
+
+const closeWindow = () => subWin.close();
 
 const $openWindow = document.getElementById('click-to-open-window');
+const $closeWindow = document.getElementById('close-window');
+
 $openWindow.addEventListener('click', openWindow);
+$closeWindow.addEventListener('click', closeWindow);
 
 window.addEventListener('message', message => {
-  console.log(message)
+  console.log(message);
 });
+
 
 export default {};
